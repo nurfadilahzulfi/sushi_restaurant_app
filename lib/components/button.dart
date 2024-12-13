d0d3948e8bf67d4d81137e7cf8b52191fa66b180
Nibras Al Afif <nibras300704@gmail.com>
2024-12-13 21:32:05 +0700

    commit nibras

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sushi_restaurant_app/theme/colors.dart';
 
 class MyButton extends StatelessWidget {
   final String text;
@@ -15,7 +16,7 @@ class MyButton extends StatelessWidget {
       onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
-          color: Color.fromARGB(189, 140, 94, 91), 
+          color: secondaryColor, 
         borderRadius: BorderRadius.circular(40)),
         padding: EdgeInsets.all(20),
         child: Row(
